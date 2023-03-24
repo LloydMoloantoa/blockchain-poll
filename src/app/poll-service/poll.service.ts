9c4a251764ffbf033f6c930b834a8dc44d2d96da
Lloyd Moloantoa <56023929+LloydMoloantoa@users.noreply.github.com>
2023-03-25 00:44:36 +0200

    web3 Service

--- src/app/poll-service/poll.service.ts
+++ src/app/poll-service/poll.service.ts
@@ -1,40 +1,66 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
 import { Poll, PollForm } from '../types';
+import { Web3Service } from '../blockchain/web3.service';
+import { fromAscii, toAscii } from 'web3-utils';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PollService {
-  onEvent(arg0: string) {
-    throw new Error('Method not implemented.');
-  }
+  constructor(private web3: Web3Service) {}
 
-  constructor() { }
+  async getPolls(): Promise<Poll[]> {
+    const polls: Poll[] = [];
 
-  getPolls(): Observable<Poll[]> {
-    return of([{
-      id: 1,
-      question : 'Do you like school or clubs',
-      thumbnail:'https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg',
-      results: [0, 5, 7],
-      options: ["Cats", "Dogs", "No"],
-      voted: true,
-    },{
-      id: 2,
-      question : 'do you like dogs or cats',
-      thumbnail:'https://images.pexels.com/photos/1118448/pexels-photo-1118448.jpeg',
-      results: [1, 6, 4],
-      options: ["A", "B", "C"],
-      voted: false,
-    }]).pipe(delay(2000));
+    const totalPolls = await this.web3.call('getTotalPolls');
+    const acc = await this.web3.getAccount();
+    const voter = await this.web3.call('getVoter', acc);
+    const voterNormalized = this.normalizeVoter(voter);
+
+    for (let i = 0; i < totalPolls; i++) {
+      const pollRaw = await this.web3.call('getPoll', i);
+      const pollNormalized = this.normalizePoll(pollRaw, voterNormalized);
+      polls.push(pollNormalized);
+    }
+
+    return polls;
   }
 
   vote(pollId: number, voteNumber: number) {
-    console.log(pollId,voteNumber);
+    this.web3.executeTransaction('vote', pollId, voteNumber);
   }
 
   createPoll(poll: PollForm) {
-    console.log(poll);
+    this.web3.executeTransaction(
+      'createPoll',
+      poll.question,
+      poll.thumbnail || '',
+      poll.options.map((opt) => fromAscii(opt))
+    );
+  }
+
+  private normalizeVoter(voter: any[][]) {
+    return {
+      id: voter[0],
+      votedIds: voter[1].map((vote) => parseInt(vote)),
+    };
+  }
+
+  private normalizePoll(pollRaw: any, voter: any): Poll {
+    return {
+      id: parseInt(pollRaw[0]),
+      question: pollRaw[1],
+      thumbnail: pollRaw[2],
+      results: pollRaw[3].map((vote: string) => parseInt(vote)),
+      options: pollRaw[4].map((opt: string) => toAscii(opt).replace(/\u0000/g, '')),
+      voted:
+        voter.votedIds.length &&
+        voter.votedIds.find((votedId: number) => votedId === parseInt(pollRaw[0])) !=
+          undefined,
+    };
+  }
+
+  onEvent(name: string) {
+    return this.web3.onEvents(name);
   }
 }
